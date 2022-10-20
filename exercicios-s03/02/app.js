4b32374e978648cd4327b03553e2d12cb47cee4d
Gustavo Maia <68251415+gugamaia@users.noreply.github.com>
2022-10-20 00:06:12 -0300

    Aula 03 e início dos exercícios

---
+++ exercicios-s03/02/app.js
@@ -0,0 +1,26 @@
+/*
+
+Em um arquivo **app.js** (incluído em **index.html**) faça o seguinte:
+
+No documento **js** faça uma **arrow function** para retornar uma mensagem (string) de **"Olá, Mundo!"**.
+
+Armazene esta **arrow function** em uma constante (**const**) de nome **mensagemOla**.
+
+Chame a função **mensagemOla** e exiba o retorno desta função no console.
+
+Ajuste a função **mensagemOla** para que receba um parâmetro **nome** e retorne a mensagem **"Olá, [nome]!"**.
+
+> Exemplo:
+
+    mensagemOla("Mariana");
+    // "Olá, Mariana!"
+
+Ao final, devemos ter uma função que monta uma mensagem de olá customizada com o nome informado por parâmetro.
+
+> Dica:
+> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+___
+Extra: Caso queira, podes experimentar criar um **campo de texto**, um **botão** e um **parágrafo** em **index.html** e fazer com que a função de nome seja executada ao pressionarmos o botão, que o nome pode seja lido do campo de texto e a mensagem seja exibida no parágrafo.
+
+*/
---
+++ exercicios-s03/03/app.js
@@ -0,0 +1,27 @@
+/*
+
+Para treinar os conceitos de Spread.
+
+Crie dois arquivos:
+- **index.html**
+- **app.js**
+
+Em **app.js** desenvolva uma função que receba **dois arrays** e realize a **concatenação** entre eles utilizando o operador **Spread**.
+Apresente o resultado no console.log(novoArray).
+
+> Exemplo:
+
+    const novoArray = concatena([1, 2, 3], [4, 5, 6]);
+    console.log(novoArray);
+    // novo array deve ser [1, 2, 3, 4, 5, 6]
+
+**Obs.:** Em **index.html** deve ser incluído o arquivo **app.js**, para executarmos o script com a função.
+
+> Dica:
+> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+
+---
+Extra: Faça com uma arrow function.
+
+*/
+
