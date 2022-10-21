27dd0d98fc6457345c3022b6cd3f3b04b3ae3cfe
Gustavo Maia <68251415+gugamaia@users.noreply.github.com>
2022-10-21 19:38:20 -0300

    arquivos

---
+++ exercicios-s03/05/script.js
@@ -0,0 +1,41 @@
+/*
+Implemente uma função "**procuraMinMax**" que receba como parâmetro um array (numérico) e identifique o **menor** e o **maior** valor do mesmo. 
+
+Imprima no console estes valores ou **"Não é possível encontrar"** quando não for possível.
+
+ Teste a função com as seguintes entradas:
+
+ - [56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47]
+ - []
+ - [1]
+ - [1, -1]
+ - null
+ - [-2,-2,-2,-2] 
+ - [20,10, 30]  
+
+___
+
+Obs.: Use let e const para declarar variáveis se for necessário.
+Extra: Faça com uma arrow function.
+*/
+
+const primeiraSeqNum = [56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47];
+const segundaSeqNum = [];
+const terceiraSeqNum [1];
+const quartaSeqNum = [1, -1];
+const quintaSeqNum = null ;
+const sextaSeqNum = [-2,-2,-2,-2]; 
+const setimaSeqNum = [20,10, 30];
+
+function procuraMinMax() {
+    
+}
+
+procuraMinMax(primeiraSeqNum);
+procuraMinMax(segundaSeqNum);
+procuraMinMax(terceiraSeqNum);
+procuraMinMax(quartaSeqNum);
+procuraMinMax(quintaSeqNum);
+procuraMinMax(sextaSeqNum);
+procuraMinMax(setimaSeqNum);
+
