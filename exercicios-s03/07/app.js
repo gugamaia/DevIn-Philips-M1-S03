fc14ec7b5ba554f4a0123301e4953b1ab9e0be4e
Gustavo Maia <68251415+gugamaia@users.noreply.github.com>
2022-10-21 21:36:02 -0300

    Exercício 07

--- exercicios-s03/07/app.js
+++ exercicios-s03/07/app.js
@@ -1,1 +1,20 @@
 console.log('Exercício 07 - Semana 03 - Módulo 01');
+
+function montaMensagem(pessoa) {
+    const { nome, idade, profissao } = pessoa;
+    return `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+  }
+  
+  const montaArrow = ({ nome, idade, profissao }) =>
+   `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+  
+  const pessoa = {
+    nome: 'Ada',
+    idade: 36,
+    profissao: 'matemática'
+  };
+  
+  const msg = montaArrow(pessoa);
+  
+  console.log(msg);
+  
