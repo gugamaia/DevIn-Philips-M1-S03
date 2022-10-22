712c2648d5d8b4e2e847a2750f92b3e20136980e
Gustavo Maia <68251415+gugamaia@users.noreply.github.com>
2022-10-21 21:09:55 -0300

    exercício 03

--- exercicios-s03/03/app.js
+++ exercicios-s03/03/app.js
@@ -25,3 +25,17 @@ Extra: Faça com uma arrow function.
 
 */
 
+console.log('Exercício 03 - Semana 03 - Módulo 01');
+
+function concatena(vetor1, vetor2) {
+  return [...vetor1, ...vetor2];
+}
+
+// equivalente em arrowfunction
+const concatenaArrow = (v1, v2) => [...v1, ...v2];
+
+const novoArray = concatena([1, 2, 3], [4, 5, 6]);
+
+console.log(novoArray);
+
+// novo array deve ser [1, 2, 3, 4, 5, 6]
