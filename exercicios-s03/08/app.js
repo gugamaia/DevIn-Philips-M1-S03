9dec3bf47221ee135bace69169eb31ece5fe6de7
Gustavo Maia <68251415+gugamaia@users.noreply.github.com>
2022-10-21 21:49:59 -0300

    Exercício 08

--- exercicios-s03/08/app.js
+++ exercicios-s03/08/app.js
@@ -1,1 +1,26 @@
 console.log('Exercício 08 - Semana 03 - Módulo 01');
+
+const uneDobraRetorna = (vetor, ...extras) => {
+    for (let i = 0; i < extras.length; i++) {
+      extras[i] *= 2;
+    }
+    //console.log({ vetor, extras});
+    return [...vetor, ...extras];
+  };
+  
+  // versao com map
+  const uneDobraRetornaMap = (vetor, ...extras) => {
+    const multi = extras.map(num => num * 2);
+    return [...vetor, ...multi];
+  };
+  
+  const r1 = uneDobraRetorna([1, 2, 3], 4, 4)
+  //console.log(r1) // [1, 2, 3, 8, 8]
+  
+  const r2 = uneDobraRetornaMap([2], 10, 4, 8) 
+  console.log(r2) // [2, 20, 8, 16]
+  
+  const r3 = uneDobraRetorna([6, 8]) 
+  //console.log(r3) // [6, 8]
+
+  
